d587b55ce64a1f845f64000b121ab877d876d7f2
jposada202020 <jquintana202020@gmail.com>
2021-03-02 20:58:56 -0500

    Initial commit

---
+++ code.py
@@ -0,0 +1,88 @@
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+
+print("ESP32 SPI hardware test")
+
+try:
+    from privados import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("ESP32 SPI webclient test")
+
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+# PICO              AIRLIFT                WIRE
+###########      ###############     ###############
+# board.GP2 ---> Airlift SCK        --- Green wire
+# board.GP3 ---> Airlift MOSI(TX)   --- Oramge wire
+# board.GP4 ---> Airlift MISO (RX)  --- Yellow Wire
+# board.GP5 ---> Airlift CS         --- Red Wire
+# board.GP6 ---> Airlift BUSY       --- Purple Wire
+# board.GP7 ---> Airlift RST        --- Blue Wire
+
+# SOURCE
+#   5V -->  VIN AIRLIFT     --- Red wire
+#  GND -->  GND AIRLIFT     --- Black Wire
+#  GND -->  PICO GND        --- White Wire
+
+esp32_cs = DigitalInOut(board.GP5)
+esp32_ready = DigitalInOut(board.GP6)
+esp32_reset = DigitalInOut(board.GP7)
+
+spi = busio.SPI(board.GP2, board.GP3, board.GP4)
+
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
+
+print("Done!")
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+print(
+    "IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com"))
+)
+print("Ping google.com: %d ms" % esp.ping("google.com"))
+
+# esp._debug = True
+print("Fetching text from", TEXT_URL)
+r = requests.get(TEXT_URL)
+print("-" * 40)
+print(r.text)
+print("-" * 40)
+r.close()
+
+print()
+print("Fetching json from", JSON_URL)
+r = requests.get(JSON_URL)
+print("-" * 40)
+print(r.json())
+print("-" * 40)
+r.close()
+
+print("Done!")
